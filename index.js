afb2c24c68e67da4e480e595acc6d878d66a7801
Varun V <varunvirupaksha321@gmail.com>
2020-01-19 12:23:35 +0530

    added visuals on pad click

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ window.addEventListener("load", () => {
   const pads = document.querySelectorAll(".pads div");
   const visual = document.querySelector(".visual");
   const colors = [
-    "#60d394",
-    "#d36060",
-    "#c060d3",
-    "#d3d160",
-    "#606bd3",
-    "#60c2d3"
+    "rgb(96, 211, 148, 0.7)",
+    "rgb(211, 96, 96, 0.7)",
+    "rgb(192, 96, 211, 0.7)",
+    "rgb(211, 209, 96, 0.7)",
+    "rgb(96, 107, 211, 0.7)",
+    "rgb(96, 194, 211, 0.7)"
   ];
 
   pads.forEach((pad, index) => {
