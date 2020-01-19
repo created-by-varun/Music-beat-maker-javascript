18087595425ef64b74959ad5777438aacbc81324
Varun V <varunvirupaksha321@gmail.com>
2020-01-19 11:53:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll.querySelectorAll(".pads div");
+
+
+    //sound
+    pads.foreach(pad => {
+        pad.addEventListener('click', function(){
+            
+        });
+    });
+});
